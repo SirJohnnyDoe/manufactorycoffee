dbbcc0a03f063b678397d3555f1a0525a046f283
aklemenchenok <aklemenchenok@flightpath3d.com>
2025-04-09 18:38:46 +0100

    to test

--- js/ui.js
+++ js/ui.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
-	/*
-	
-	$('.panel-pointer').click(function () { 
+    $('.slider-nav .arrow').click(function () {
+        $('.slider-list .slide').toggleClass('active');
+    });
+    /*
+
+	$('.panel-pointer').click(function () {
 		$('.section').removeClass('editable');
 		$('.section').toggleClass('opened');
 		$(this).toggleClass('active');
 	});
-	
+
 	*/
 });
