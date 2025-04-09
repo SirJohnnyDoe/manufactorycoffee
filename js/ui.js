e56110c4a0a25859bef9203ddbf4702c945568a7
SirJohnnyDoe <85226532+SirJohnnyDoe@users.noreply.github.com>
2025-04-09 12:57:43 +0100

    Add files via upload

---
+++ js/ui.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	/*
+	
+	$('.panel-pointer').click(function () { 
+		$('.section').removeClass('editable');
+		$('.section').toggleClass('opened');
+		$(this).toggleClass('active');
+	});
+	
+	*/
+});
